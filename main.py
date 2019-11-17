490a61bd3e643d1114bdce809309b0e495aa635d
Dmitry Skorykh <dmitry.skorykh@emlid.com>
2019-11-17 03:18:52 +0300

    be: add no cache header

--- main.py
+++ main.py
@@ -362,6 +362,17 @@ def video_dash():
     
     return render_template('video_dash.html', emo=emotion_label(emotion), emo_other = emotion_label(emotion_other), prob = emo_prop(df_2), prob_other = emo_prop(df))
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 @sockets.route('/stream')
 def stream(ws):
